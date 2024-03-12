8d1becf239dddddafa0eb6eb09795be62ac9be67
AdrianWolfP <wolfy52032@gmail.com>
2024-03-12 13:05:11 -0500

    Worked and checking with the checker

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+""" Write a script that states flask web app"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def greeting():
+    """ Returns a greeting """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ function that returns hbnb """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ function that returns message with a C """
+    new_text = text.replace("_", " ")
+    return ('C ' + new_text)
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def textPython(text='is cool'):        
+    return ('Python ' + text.replace('_', ' '))
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return ("{} is a number".format(n))
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
