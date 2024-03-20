1cd9f72d43771d59fcdb49170f1bbc88c745a3b3
Wolfgang Pendergass <119448680+AdrianWolfP@users.noreply.github.com>
2024-03-20 11:09:57 -0500

    pep8 fixing

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -27,8 +27,35 @@ def c_text(text):
 
 @app.route('/python/<hiss>', strict_slashes=False)
 @app.route('/python', strict_slashes=False)
-def 
+def hiss(hiss="is cool"):
+    """ function that reutrns a python message """
+    return 'Python {}'.format(hiss.replace('_', ' '))
 
+
+@app.route('/number/<input>', strict_slashes=False)
+def digis(input):
+    """ function that displays a number if it is an integer """
+    if input.isdigit():
+        return '{} is a number'.format(input)
+
+
+@app.route('/number_template/<int:input>', strict_slashes=False)
+def html_template(input):
+    """ function that displays a HTML page only if input is an integer """
+    return render_template('5-number.html', number=input)
+
+
+@app.route('/number_odd_or_even/<int:input>', strict_slashes=False)
+def odd(input):
+    """ function that displays an HTML page that checks odd or even """
+    if input % 2 == 0:
+        input = '{} is even'.format(input)
+    else:
+        input = '{} is odd'.format(input)
+    return render_template('6-number_odd_or_even,html', can_you_even=input)
+
+
+@app.route('/states_list', strict_slashes=False)
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
